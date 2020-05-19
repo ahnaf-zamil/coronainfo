c9e76c96636f32ced02c33aefc6baefdbef9fba3
ahnaf_zamil <ahnafzamil896@gmail.com>
2020-05-19 14:00:43 +0600

    Updates

---
+++ precache-manifest.3d23bef77f1a1b2ba35d35d1cf04bc6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b16857a40a350c245acf002241a22c",
+    "url": "/coronainfo/index.html"
+  },
+  {
+    "revision": "304d33db8bb797aacdc1",
+    "url": "/coronainfo/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "f3374fbe5310029be74d",
+    "url": "/coronainfo/static/css/main.96b57a68.chunk.css"
+  },
+  {
+    "revision": "304d33db8bb797aacdc1",
+    "url": "/coronainfo/static/js/2.8a19bfdf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/coronainfo/static/js/2.8a19bfdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3374fbe5310029be74d",
+    "url": "/coronainfo/static/js/main.0336a0a1.chunk.js"
+  },
+  {
+    "revision": "7b18d0c8b850185ba2a6",
+    "url": "/coronainfo/static/js/runtime-main.ccb8fe0f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/coronainfo/precache-manifest.3d23bef77f1a1b2ba35d35d1cf04bc6d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coronainfo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
